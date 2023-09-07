c74116e9c2a34080c655e0373dbb29f31a1f3aec
Appinventiv <appinventiv@Shubham-MacBook-Pro.local>
2023-09-07 12:56:54 +0530

    redux imp

--- App.tsx
+++ App.tsx
@@ -9,26 +9,26 @@ import SuT from './src/Nested Navigation/SuT'; // Stack under tab
 import TuS from './src/Nested Navigation/TuS'; // tab under stack
 import SuD from './src/Nested Navigation/SuD'; //stack under drawer
 //UnComment one at a Time
-const App = () => {
-  return (
-    <NavigationContainer>
-      {/* <Tabs /> */}
-      {/* <Stack /> */}
-      <Drawer />
-    </NavigationContainer>
-  );
-};
+// const App = () => {
+//   return (
+//     <NavigationContainer>
+//       {/* <Tabs /> */}
+//       {/* <Stack /> */}
+//       <Drawer />
+//     </NavigationContainer>
+//   );
+// };
 
 import ReduxCounter from './src/Component/ReduxCounter';
 import Counter from './src/Component/Counter';
 import {Provider} from 'react-redux';
 import store from './src/Redux/Store';
 
-// const App = () => {
-//   return (
-//     <Provider store={store}>
-//       <ReduxCounter />
-//     </Provider>
-//   );
-// };
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Counter />
+    </Provider>
+  );
+};
 export default App;
